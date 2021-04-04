b88fe5103fdd3943012497c02c422a71d536993e
戸田優也 <todayuuya@Tymac.local>
2021-04-05 01:40:58 +0900

    7th commit

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,3 @@ module Ryoken
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
-    :user_name => ENV["MAIL_USERNAME"],
-    :password => ENV["MAIL_PASSWORD"],
+    :user_name => ENV['MAIL_USERNAME'],
+    :password => ENV['MAIL_PASSWORD'],
     :authentication => 'login'
   }
 end
