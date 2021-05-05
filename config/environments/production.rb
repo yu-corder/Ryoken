4c8c48b9dbec5b1203e3283dbfb5ef72e20f14cc
戸田優也 <todayuuya@Tymac.local>
2021-05-05 20:20:56 +0900

    update 22

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
 
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST']}
+  config.action_mailer.default_url_options = ENV['MAIL_HOST']
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
