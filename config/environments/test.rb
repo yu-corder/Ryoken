d5abff7ad91475fbcf84207bcec6a28e3ce265db
戸田優也 <todayuuya@Tymac.local>
2021-05-06 23:24:51 +0900

    update 36

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   host = ENV['MAIL_HOST']
   
 
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'localhost'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
