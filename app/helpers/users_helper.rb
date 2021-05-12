ea4091cac7a079a8b99c3dd787fd3ac22c8285b6
戸田優也 <todayuuya@Tymac.local>
2021-05-12 23:28:52 +0900

    update 53

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def resource_name
+        :account
+     end
+     
+     def resource
+        @resource ||= Account.new
+     end
+     
+     def devise_mapping
+        @devise_mapping ||= Devise.mappings[:account]
+     end
 end
