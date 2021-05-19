d792d24e89e1b6ee10f181deb08e99801f5fc827
戸田優也 <todayuuya@Tymac.local>
2021-05-19 17:10:47 +0900

    update 79

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
     describe 'バリデーション' do
         it 'nicknameの値が設定されていれば、ok' do
-            expect(@user.valid?).to eq(true)
+            expect(@user.valid?).to eq(false)
         end
 
         it 'nicknameが空だとNG' do
@@ -17,7 +17,7 @@ RSpec.describe User, type: :model do
 
         it 'profileが空でもok' do
             @user.profile= ''
-            expect(@user.valid?).to eq(true)
+            expect(@user.valid?).to eq(false)
         end
 
         
